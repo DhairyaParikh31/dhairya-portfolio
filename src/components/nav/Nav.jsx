d83ad842caf9dc0a980628725e7822b8da8a5186
DhairyaParikh31 <dhairyaparikh31@gmail.com>
2024-05-05 19:19:00 +0530

    navbar completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import './nav.css'
+import React, { useState, useEffect } from 'react';
+import './nav.css';
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { FaLaptopCode } from "react-icons/fa6";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const [activeTab, setActiveTab] = useState("home");
 
-export default Nav
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      let currentSection = '';
+
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+          currentSection = section.id;
+        }
+      });
+
+      setActiveTab(currentSection || 'home'); // Set default to 'home' if no section is in view
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    // Cleanup function to remove the event listener when component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav>
+      <a href="#" className={activeTab === "home" ? 'active' : ''} onClick={() => handleTabClick("home")}><AiOutlineHome/></a>
+      <a href="#about" className={activeTab === "about" ? 'active' : ''} onClick={() => handleTabClick("about")}><AiOutlineUser/></a>
+      <a href="#experience" className={activeTab === "experience" ? 'active' : ''} onClick={() => handleTabClick("experience")}><BiBook/></a>
+      <a href="#services" className={activeTab === "services" ? 'active' : ''} onClick={() => handleTabClick("services")}><FaLaptopCode/></a>
+      <a href="#contact" className={activeTab === "contact" ? 'active' : ''} onClick={() => handleTabClick("contact")}><BiMessageSquareDetail/></a>
+    </nav>
+  );
+};
+
+export default Nav;
